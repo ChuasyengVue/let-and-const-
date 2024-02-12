72ab5907e85b7ebb7c0c819234464b13aab05129
Chuasyeng Vue <152819023+ChuasyengVue@users.noreply.github.com>
2024-02-12 16:17:02 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+// What is the difference between (var) and (let)?
+// You can reassign and redclare with var but not redeclare with let.
+
+// What is the difference between (var) and (const)?
+// You can reassign and redeclare with var but not with const.
+
+// What is the difference between (let) and (const)?
+// You can reassign with let, but cant redeclare or reassign with const.
+
+// What is hoisting?
+// It explain how JS compiler works.
